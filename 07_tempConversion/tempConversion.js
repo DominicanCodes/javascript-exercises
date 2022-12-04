f0c07b2362e77cfec0389aa498229878a1e7a79d
Andres Cardenas <65802352+DominicanCodes@users.noreply.github.com>
2022-12-04 13:23:37 -0500

    Test PASSED for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = (fTemp - 32) * (5/9)
+  cTemp = +(Math.round(cTemp + "e+1")  + "e-1");
+  return cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = (cTemp * 9/5) + 32
+  fTemp = +(Math.round(fTemp + "e+1")  + "e-1");
+  return fTemp
 };
 
 // Do not edit below this line
